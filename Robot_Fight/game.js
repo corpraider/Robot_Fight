41ebca26f3035944d4aa76084821b711ab11ccdb
JP <mamuricj@gmail.com>
2021-06-24 21:39:32 -0500

    updated logic

--- Robot_Fight/game.js
+++ Robot_Fight/game.js
@@ -1,20 +1,41 @@
-// this creates a function named "fight"
+// Updated to get the game started 
 
-function fight() {
-    window.alert("The fight has begun!");
+var playerName = window.prompt("what is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+
+// You can also log multiple values at once like this 
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roberto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+
+  //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+
+    enemyHealth = enemyHealth - playerAttack;
+
+  // Log a resulting message to the console so we know that it worked.
+
+    console.log (
+        playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyName + " health remaining. "
+    );
+  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+
+  playerHealth = playerHealth - enemyAttack;
+
+  // Log a resulting message to the console so we know that it worked.
 }
 
-var playerName = window.prompt("What is your robot's name?");
-// Note the lack of quotation marks around PlayerName
+// check to see if the value of playerHealth variable is greater than 0
 
- console.log(playerName)
-
-console.log("This logs a string, good for leaving yourself a message");
-//this will do math and log 20
-console.log(10+10);
-// what is this?
-
-console.log("Our robot's name is " + playerName);
-
-// fight();
-
+if (playerHealth > 0) {
+    console.log("Your Player is still Alive")
+}
+// execution of the function fight
+fight();
